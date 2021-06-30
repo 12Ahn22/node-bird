80847659670f982fdae481b80f89d37da538fa5a
ayo <12ayo22@gmail.com>
2021-06-30 20:51:25 +0900

    add: layout page & css

--- app.js
+++ app.js
@@ -3,22 +3,26 @@ const path = require('path');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 const session = require('express-session');
+const cookieParser = require('cookie-parser');
+// view 엔진
+const nunjucks = require('nunjucks');
 
 dotenv.config();
 
 // router
-// const pageRouter = require('./routes/page');
-const exp = require('constants');
-const { urlencoded } = require('express');
-const cookieParser = require('cookie-parser');
+const pageRouter = require('./routes/page');
 
 // create application
 const app = express();
 // setting port
 app.set('port', process.env.PORT || 5000);
 // setting view engines
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views')); // views의 기본 경로를 지정해준다
+app.set('view engine', 'html');
+// app.set('views', path.join(__dirname, 'views')); // views의 기본 경로를 지정해준다
+nunjucks.configure('views',{
+  express:app,
+  watch:true,
+});
 
 // use morgan
 app.use(morgan('dev')); // 로그를 찍음
@@ -27,7 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // express.json() 사용하기 - 데이터를 json 형식으로 사용할 수 있게 해준다.
 app.use(express.json());
 // url을 인코딩해주는 미들웨어. extended: false는 내장 모듈을 사용하겠다는 의미이다.
-app.use(urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 // 쿠키 파서 사용하기
 app.use(cookieParser(process.env.COOKIE_SECRET));
 // 세션 사용하기
@@ -44,12 +48,11 @@ app.use(
 );
 
 // 라우터 경로
-// app.use('/', pageRouter); // 루트는 pageRouter
+app.use('/', pageRouter); // 루트는 pageRouter
 
-// 에러 처리를 위한 미들웨어
-// 이 미들웨어는 모든 요청에서 실행된다.
+// 404 응답을 하는 미들웨어
 app.use((req, res, next) => {
-  console.log('모든 요청에서 실행되나요?');
+  console.log('404 에러 응답 미들웨어입니다.');
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다`);
   error.status = 404;
   next(error); // 에러를 다음으로 전달해준다.
--- routes/page.js
+++ routes/page.js
@@ -1,1 +1,47 @@
 // page 라우터
+const express = require('express');
+const router = express.Router();
+
+// 미들웨어
+// 미들웨어는 use 메서드를 사용한다.
+// 이 미들웨어는 localhost:5000/에 접근 했을 때,
+// 항상 실행되는 미들웨어이다.
+router.use((req,res,next)=>{
+  console.log('미들웨어입니다.')
+  // res.locals는 req가 들어왔을 때에만 살아있는 데이터이다.
+  // 모든 템플릿 엔진이 사용하는 변수라서 res.locals에 선언한다.
+  res.locals.user = null;
+  res.locals.followerCount = 0;
+  res.locals.followingCount = 0;
+  res.locals.followerIdList = [];
+  next();
+});
+
+
+// GET 메서드
+// localhost:5000/
+router.get('/',(req,res,next)=>{
+  // 트윗
+  const twits = [];
+
+  res.render('main',{
+    title: 'NodeBird',
+    twits,
+  });
+});
+
+// GET 메서드
+// localhost:5000/profile  
+router.get('/profile',(req,res)=>{
+  res.render('profile',{title: '내 정보 - NordBird'});
+});
+
+// GET 메서드
+// localhost:5000/join
+router.get('/join',(req,res)=>{
+  res.render('join',{title : '회원 가입 - NordBird'});
+});
+
+
+// 라우터 내보내기
+module.exports = router;
